d17753b86ead657a375ddf2fc53ef85378cf5f58
Haobo-Wang <wanghaobo1120@gmail.com>
2026-03-12 18:25:03 -0400

    Adjusted Schema for staff to manage events

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -38,6 +38,9 @@ export default function EventCard({
   const canDelete =
     currentUser?.role === "ORGANIZER" && currentUser?.id === event.organizer.id;
 
+  const canManage = 
+    (currentUser?.role === "ORGANIZER" && currentUser?.id === event.organizer.id || currentUser?.role === "STAFF" && currentUser?.id === event.staff.id);
+
   async function handleDelete() {
     const token = localStorage.getItem("token");
 
@@ -79,6 +82,9 @@ export default function EventCard({
     }
   }
 
+  async function handleManageEvent(){
+      router.push(`/events/${event.id}/dashboard`)
+    }
 
   async function handlePurchase() {
     setLoading(true);
@@ -137,6 +143,8 @@ export default function EventCard({
       setErrorMessage("Network error. Please try again.");
       setLoading(false);
     }
+
+    
   }
 
   return (
@@ -171,14 +179,21 @@ export default function EventCard({
               {event.organizer.name}
             </p>
           </div>
-
-          {canDelete ? (
+         {canDelete ? (
             <button
               onClick={handleDelete}
               disabled={deleteLoading}
-              className="shrink-0 rounded-lg bg-red-600 px-4 py-2 text-sm font-medium text-white disabled:opacity-50"
+              className="shrink-0 rounded-lg bg-red-600 p-3 text-white transition hover:bg-red-700 disabled:opacity-50"
+              aria-label="Delete event"
+              title="Delete event"
             >
-              {deleteLoading ? "Deleting..." : "Delete Event"}
+              {deleteLoading ? (
+                <span className="text-sm font-medium">...</span>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+              </svg>
+              )}
             </button>
           ) : null}
         </div>
@@ -206,13 +221,24 @@ export default function EventCard({
           </div>
         ) : null}
 
-        <button
+        
+        <div className="mt-4 flex item-center justify-between">
+          <button
           onClick={handlePurchase}
           disabled={loading || event.ticketTiers.length === 0 || showSuccessPopup}
-          className="mt-4 rounded-lg bg-black px-4 py-2 text-white disabled:opacity-50"
+          className="mt-4 rounded-lg bg-black px-4 py-2 text-white transition hover:bg-gray-600 disabled:opacity-50"
         >
           {loading ? "Processing..." : "Purchase"}
         </button>
+          {canManage ? (
+            <button
+              onClick={handleManageEvent}
+              className="mt-4 rounded-lg bg-green-600 px-4 py-2 font-medium text-white hover:bg-green-700"
+            >
+              Manage
+            </button>
+            ) : null}
+          </div>
       </div>
     </>
   );
